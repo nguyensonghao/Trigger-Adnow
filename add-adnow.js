7484315fe072c247d2f5ef83d0c0681c04f9d847
nguyensonghao94 <20121630@student.hut.edu.vn>
2019-08-26 22:52:34 +0700

    Fix bug

---
+++ add-adnow.js
@@ -0,0 +1,5 @@
+$('#ADS_130_15s_container').html('<div id="SC_TBlock_681951" class="SC_TBlock">loading...</div> ');
+(sc_adv_out = window.sc_adv_out || []).push({
+    id : "681951",
+    domain : "n.domnovrek.com"
+});
--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const sleep = (time) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(`sleep ${time}`);
+            resolve(true);
+        }, time);
+    })
+}
+
+const writeContent = (content) => {
+    return new Promise((resolve) => {
+        fs.writeFile('./test.txt', content, (err) => {
+            resolve(true);
+        })
+    })
+}
 
 (async () => {
-    const browser = await puppeteer.launch({
-        ignoreHTTPSErrors: true,
-        args: [ '--proxy-server=http=190.157.247.65:53281' ]
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://google.com');
+    await page.setViewport({
+        width: 1680,
+        height: 1050,
+    })
+    await page.goto('https://eva.vn');
+    await page.addScriptTag({ path: './add-adnow.js' });
+    await page.addScriptTag({ url: 'https://st-n.ads1-adnow.com/js/a.js' });
+    await sleep(10000);
+    let bodyHTML = await page.evaluate(() => document.head.innerHTML);
+    await writeContent(bodyHTML);
     await page.screenshot({path: 'example.png'});
     await browser.close();
 })();
